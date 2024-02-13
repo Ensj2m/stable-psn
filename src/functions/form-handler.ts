e13fa4984e201fb19906e56b57ebf305fd2d97fd
systematicRealm <119763173+systematicRealm@users.noreply.github.com>
2024-02-14 02:51:51 +0300

    👌 IMPROVE: Commented Something

--- src/functions/form-handler.ts
+++ src/functions/form-handler.ts
@@ -3,23 +3,23 @@ import { FormEvent } from "react";
 export async function formHandler(event: FormEvent<HTMLFormElement>) {
   event.preventDefault();
 
-  const sku = event.currentTarget.sku.value;
-  const token = event.currentTarget.token.value;
+  // const sku = event.currentTarget.sku.value;
+  // const token = event.currentTarget.token.value;
 
-  const formData = {
-    sku,
-    token,
-    region: "ar-AE",
-    add: "Submit",
-  };
+  // const formData = {
+  //   sku,
+  //   token,
+  //   region: "ar-AE",
+  //   add: "Submit",
+  // };
 
-  try {
-    const response = await fetch("/api/submit", {
-      method: "POST",
-      body: JSON.stringify(formData),
-    });
-    console.log("Request successful");
-  } catch (error) {
-    console.error("Error:", error);
-  }
+  // try {
+  //   const response = await fetch("/api/submit", {
+  //     method: "POST",
+  //     body: JSON.stringify(formData),
+  //   });
+  //   console.log("Request successful");
+  // } catch (error) {
+  //   console.error("Error:", error);
+  // }
 }
